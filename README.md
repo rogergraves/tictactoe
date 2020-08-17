d136c276c8084ea0867f2b713e8af9c70d7630ef
Roger Graves <rogergraves@gmail.com>
2020-08-17 11:27:12 -0700

    Add listener that triggers anytime a user clicks on one of the main buttons

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple Javascript Tic Tac Toe
+
+#### Instructions
+1. Copy/paste index.htm, app.js and style.css into your working directory.
+2. Create functions that get run whenever someone clicks on a button.
+3. 
--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   // Put your Javascript code below this line.
-  
 
+
+  // LISTENERS -- these functions trigger when users do things
+  $('.tic').click(function(e) {
+    // This will run whenever anyone clicks on something with a 'tic' class.
+
+    // This shows the ID of which of the buttons got clicked
+    console.log("This was clicked:", e.target.id);
+  });
 });
