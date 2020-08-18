b9932462f400e7bd832385378044ee1bc7e7e288
Roger Graves <rogergraves@gmail.com>
2020-08-17 18:12:22 -0700

    When users click on '#' cells it replaces it with an X, otherwise it doesn't do anything

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ $(document).ready(function(){
   $('.tic').click(function(e) {
     // This will run whenever anyone clicks on something with a 'tic' class.
 
+    const cell = $(e.target);
+    record_move_if_allowed(cell);
+
     // This shows the ID of which of the buttons got clicked
     console.log("This was clicked:", e.target.id);
   });
+
+  function record_move_if_allowed(cell) {
+    if(cell.html() === '#') {
+      // All good!
+      cell.html('X');
+    } else {
+      // Nah... They clicked on something that already had an X or an O
+      alert('No way hombre... That is CHEATING!');
+    }
+  }
 });
